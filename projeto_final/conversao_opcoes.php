d3de2a53f65a88dfe0bd5454e421839b8d5d59f9
KaleoMariz <kaleomariz@gmail.com>
2025-11-18 11:44:20 -0300

    Update conversao_opcoes.php
    adicionando opção do usuário dar sugestões

--- projeto_final/conversao_opcoes.php
+++ projeto_final/conversao_opcoes.php
@@ -20,16 +20,26 @@
     </head>
 <body>
 <header>
-   <img src="choose_your_currency.jpg" width="500px">
+   <img src="ilustracoes/choose_your_currency.jpg" width="500px">
 
 </header>   
 
 <section>
     <a href="conversao_do_real.php"><h1>Clique aqui para converter o real para outras moedas</h1></a>
     <a href="conversao_do_dolar.php"><h1>Clique aqui pra converter o dólar para outras moedas</h1></a>
-    <a href="conversao_do_ouro.php"><h1>Clique aqui para converter o ouro para outras moedas</h1></a><br>
+    <a href="conversao_do_ouro.php"><h1>Clique aqui para converter o ouro para outras moedas</h1></a><br><br><br>
+
+    <form>
+    <label><h2> Se queres sugestões para melhorar nossa página, por favor escreva aqui abaixo e envie sua sugestão.<h2></label>
+    <textarea name="sugestao"> Escreva aqui sua sugestão.</textarea><br>
+    <input type="submit" value ="Clique aqui para enviar sua sugestão">
+    <br><br>
+
+
+    </form>
+
+
     <a href="index.php"><h2> Voltar à página principal.</h2></a>
 </section>
 </body>
-
 </html>
