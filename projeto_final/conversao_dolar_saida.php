a49fba3a4cd56d065a5c8269d54ddda35ed4decb
KaleoMariz <kaleomariz@gmail.com>
2025-11-18 17:52:52 -0300

    Update conversao_dolar_saida.php

--- projeto_final/conversao_dolar_saida.php
+++ projeto_final/conversao_dolar_saida.php
@@ -5,32 +5,32 @@ $conversao_dolar =$_GET['conversao_dolar'];
  switch($conversao_dolar){
         case 1: 
             $valor_do_dolar = $dolar * 5.32;
-            echo " $dolar dólares equivale a $valor_do_dolar reais.";
+            echo "<h1> $dolar dólares equivale a $valor_do_dolar reais.</h1>";
             break;
         
         case 2: 
             $valor_do_dolar = $dolar * 0.76;
-            echo " $dolar dólares equivale a $valor_do_dolar libras inglesas.";
+            echo "<h1> $dolar dólares equivale a $valor_do_dolar libras inglesas.</h1>";
             break;
 
         case 3: 
             $valor_do_dolar =$dolar * 0.86;
-            echo "$dolar dólares equivale a $valor_do_dolar euros.";
+            echo " <h1>$dolar dólares equivale a $valor_do_dolar euros.</h1>";
             break;
 
         case 4: 
             $valor_do_dolar =$dolar * 7.12;
-            echo "$dolar dólares equivale a $valor_do_dolar yuanes chineses.";
+            echo "<h1>$dolar dólares equivale a $valor_do_dolar yuanes chineses.</h1>";
             break;
         
         case 5:
             $valor_do_dolar = $dolar * 154.09;
-            echo "$dolar dólares equivale a $valor_do_dolar yenes japoneses.";
+            echo "<h1>$dolar dólares equivale a $valor_do_dolar yenes japoneses.</h1>";
             break;
         
         default: 
             $valor_do_dolar = $dolar * 0.0076; 
-            echo "$dolar dólares equivale a $valor_do_dolar gramas de ouro."; 
+            echo "<h1>$dolar dólares equivale a $valor_do_dolar gramas de ouro.</h1>"; 
             break;
             
     }
