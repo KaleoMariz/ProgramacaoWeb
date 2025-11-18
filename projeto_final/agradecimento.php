b39b7ce3ee56366838ed626cae974c0e53f64cce
KaleoMariz <kaleomariz@gmail.com>
2025-11-18 12:05:31 -0300

    Add files via upload

---
+++ projeto_final/agradecimento.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title> Obrigado!</title>
+
+</head>
+<body>
+<section style="text-align: center">
+<h1>Agradecemos a sua sugestão!</h1>
+
+<a href="conversao_opcoes.php">Voltar para o menu de opções</a>
+</section>
+
+
+<body>
+</html>
